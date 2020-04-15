d95536069c6d36824405b7bc4995f9f55124fcbb
Hayring <KID141220@gmail.com>
2020-04-15 23:45:28 +0800

    add BinarySearch
    +BinarySearch

---
+++ binarysearch.go
@@ -0,0 +1,25 @@
+package main
+
+func BinarySearch(sortedNums []int, target int) int {
+	length := len(sortedNums)
+	if length == 0 {
+		return  -1
+	}
+	if length == 1 {
+		if sortedNums[0] == target {
+			return 0
+		} else {
+			return -1
+		}
+	}
+	mid := length/2
+	if sortedNums[mid] == target {
+		return mid
+	}
+	if target < sortedNums[mid] {
+		return BinarySearch(sortedNums[:mid], target)
+	} else {
+		return BinarySearch(sortedNums[mid + 1:], target) + mid + 1
+	}
+
+}
